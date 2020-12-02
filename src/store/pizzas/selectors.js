10b22752473a7eeaf74bfadef8f5c0e0ff95cf63
Domenico Gemoli <domenicogemoli@gmail.com>
2020-12-02 17:08:07 +0100

    Sort pizzas by popularity

--- src/store/pizzas/selectors.js
+++ src/store/pizzas/selectors.js
@@ -1,1 +1,8 @@
-export const selectPizzas = reduxState => reduxState.pizzas.allPizzas;
+export const selectPizzas = reduxState => {
+  // I need to clone my array because otherwise .sort will edit the state directly!
+  const clonedPizzas = [...reduxState.pizzas.allPizzas];
+
+  return clonedPizzas.sort((a, b) => {
+    return b.bought - a.bought;
+  });
+};
