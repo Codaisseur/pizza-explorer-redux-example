20d34723cc1958fabce43f5bd95ab95f8d1090fe
Domenico Gemoli <domenicogemoli@gmail.com>
2020-12-02 17:31:25 +0100

    Add some styling

--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -4,30 +4,41 @@ import { selectUser } from "../store/user/selectors";
 import { selectPizzas } from "../store/pizzas/selectors";
 import { toggleFavorite } from "../store/user/actions";
 
+import "./PizzaList.scss";
+
 export default function PizzaList() {
   const dispatch = useDispatch();
   const user = useSelector(selectUser);
   const pizzas = useSelector(selectPizzas);
 
   return (
-    <div>
+    <div className="PizzaList">
       <h1>Pizza Explorer</h1>
       <p>
         Welcome back, <strong>{user.name}</strong>! There are{" "}
         <strong>{pizzas.length}</strong> pizzas in total:
       </p>
-      <ul>
+      <ul className="Pizzas">
         {pizzas.map(pizza => (
-          <li key={pizza.id}>
+          <li
+            key={pizza.id}
+            className="Pizza"
+            style={{ backgroundImage: `url(${pizza.image})` }}
+          >
             <button
               onClick={() => {
                 dispatch(toggleFavorite(pizza.id));
               }}
+              className={`FavToggle ${
+                user.favorites.includes(pizza.id) ? "fav" : ""
+              }`}
             >
               {user.favorites.includes(pizza.id) ? "♥" : "♡"}
             </button>
-            <strong>{pizza.name}</strong> ({pizza.description}) <br />
-            <em>Bought {pizza.bought} times</em>
+            <div className="Overlay">
+              <strong>{pizza.name}</strong> ({pizza.description}) <br />
+              <em>Bought {pizza.bought} times</em>
+            </div>
           </li>
         ))}
       </ul>
