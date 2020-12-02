f16e86ab6c1bbe34805c54f182f9a7bbc0929167
Domenico Gemoli <domenicogemoli@gmail.com>
2020-12-02 17:25:21 +0100

    Implement toggleFavorite in user reducer

--- src/store/user/reducer.js
+++ src/store/user/reducer.js
@@ -7,6 +7,18 @@ const initialState = {
 
 export default function reducer(state = initialState, action) {
   switch (action.type) {
+    case "user/toggleFavorite": {
+      const id = action.payload;
+
+      const favorites = state.favorites.includes(id) // check if we already have it
+        ? state.favorites.filter(f => f !== id) // remove
+        : [...state.favorites, id]; // add
+
+      return {
+        ...state,
+        favorites,
+      };
+    }
     default: {
       return state;
     }
