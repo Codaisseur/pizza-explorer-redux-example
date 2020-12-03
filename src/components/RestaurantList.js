232dd463bab046fa2bdec77f31c9b3958e3e362a
Domenico Gemoli <domenicogemoli@gmail.com>
2020-12-03 10:33:56 +0100

    Add selectRestaurantsWithPizzas

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,16 +1,23 @@
 // src/components/PizzaList.js
 import { useSelector } from "react-redux";
-import { selectRestaurants } from "../store/restaurants/selectors";
+import { selectRestaurantsWithPizzas } from "../store/selectors";
 
 export default function RestaurantList() {
-  const restaurants = useSelector(selectRestaurants);
+  const restaurants = useSelector(selectRestaurantsWithPizzas);
 
   return (
     <div>
       <h1>Restaurants</h1>
       <ul>
         {restaurants.map(restaurant => (
-          <li key={restaurant.id}>{restaurant.name}</li>
+          <li key={restaurant.id}>
+            {restaurant.name}
+            <ul>
+              {restaurant.pizzas.map(pizza => (
+                <li key={pizza.id}>{pizza.name}</li>
+              ))}
+            </ul>
+          </li>
         ))}
       </ul>
     </div>
---
+++ src/store/selectors.js
@@ -0,0 +1,19 @@
+export const selectRestaurantsWithPizzas = reduxState => {
+  // .map returns a new (cloned) array so I don't have to worry about cloning it
+  return reduxState.restaurants.allRestaurants
+    .map(restaurant => {
+      // replace each restaurant with a new object...
+      return {
+        // ...that contains everything from the original restaurant...
+        ...restaurant,
+        // ...but also replaces the pizzas array...
+        pizzas: restaurant.pizzas.map(pizzaId =>
+          // ...so that it contains a list of pizzas instead of a list of ids
+          reduxState.pizzas.allPizzas.find(pizza => pizza.id === pizzaId)
+        ),
+      };
+    })
+    .sort((a, b) => {
+      return a.name.toLowerCase().localeCompare(b.name.toLowerCase());
+    });
+};
